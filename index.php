a0c2946797ae0edf41901f4cb715aa4d19ef97cb
Sam Gregory <samuel@digital-detox.co.uk>
2015-05-21 19:29:46 +0100

    SG: Updated style and format for better UX

--- index.php
+++ index.php
@@ -1,47 +1,34 @@
 <!DOCTYPE>
 <html>
 <head>
-  <title>Test page</title>
-
+  <title>Browser Testing</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <style type="text/css">
-    .wrap {
-      max-width: 970px;
-      background: #ccc; 
-      margin:0 auto;
-    }
-    .access {
-      position: absolute;
-      left: -9999px;
-    }
-  </style>
-
   <div class="wrap">
-    <h1>Browser testing</h1>
-    <p>Here i am testing </p>
+    <h1>Browser Testing</h1>
+    <p>Here i am testing what a user is ACTUALLY using Vs what the browser tells me it is and log this information.</p>
+    <p>Please fill in as much information as you can below...</p>
     <form action="log.php">
       <label for="device">Device</label>
       <input type="text" id="device" name="device"/>
       <br>
       <label for="browser">Browser (Optional)</label>
       <input type="text" id="browser" name="browser"/>
-
-      <input type="checkbox" selected="selected" class="access" name="checkbox"/>
-
+      <input type="checkbox" class="access" name="checkbox"/>
       <input type="hidden" value="" name="window" id="widow-width">
-      
+      <br>
       <input type="submit">
     </form>
-    <p><b>Window width: </b>
-    <script type="text/javascript">
-        var windowWidth = window.innerWidth || screen.width,
-            d = document;
-      d.write(windowWidth);
-      d.getElementById('widow-width').value = windowWidth;
-    </script>
-    </p>
+    <footer>
+      Website created by Sam Gregory.
+    </footer>
   </div>
+  <script type="text/javascript">
+      var windowWidth = window.innerWidth || screen.width,
+          d = document;
+    d.getElementById('widow-width').value = windowWidth;
+  </script>
 </body>
 </html>
--- log.php
+++ log.php
@@ -1,53 +1,55 @@
+<?php 
+  $useragent = $_SERVER['HTTP_USER_AGENT'] . "\r\n";
+  $device = $_GET['device'];
+  $browser = $_GET['browser'];
+
+  if ($GET['checkbox'] === false) {
+    if (strlen($device)>0) { $message = "Device: " . $device . "\r\n"; }
+    if (strlen($browser)>0) { $message = $message . "Browser: " . $browser . "\r\n"; }
+    $message = $message . "Window Width: " . $_GET['window'] . "\r\n";
+    $message = $message . "User Agent: " . $useragent . "\r\n"; 
+    $message = $message .  "--------------\r\n";
+            
+    file_put_contents("log.txt", $message, FILE_APPEND);
+  }
+
+?>
+
 <!DOCTYPE>
 <html>
 <head>
-  <title>Test page</title>
-
+  <title>Browser Testing</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <style type="text/css">
-    .wrap {
-      max-width: 970px;
-      background: #ccc; 
-      margin:0 auto;
-    }
-  </style>
-
   <div class="wrap">
-    <h1>Here is some stuff to send</h1>
-    <p>It has also been logged!</p>
-    <p>
-      <?php 
-          $useragent = $_SERVER['HTTP_USER_AGENT'] . "\r\n";
-          $device = $_GET['device'];
-          $browser = $_GET['browser'];
-
-          if (strlen($device)>0) { $message = "Device: " . $device . "\r\n"; }
-          if (strlen($browser)>0) { $message = $message . "Browser: " . $browser . "\r\n"; }
-          $message = $message . "Window Width: " . $_GET['window'] . "\r\n";
-          $message = $message . "User Agent: " . $useragent . "\r\n"; 
-          $message = $message .  "--------------\r\n";
-          
-          file_put_contents("log.txt", $message, FILE_APPEND);
-         ?>
-    </p>
-    <p>
+    <h1>Browser Testing - Results</h1>
+    <p>Here you can see what the browser is telling me it is Vs the information you know yourself to be true.</p>
+    <div class="results">
+      <?php if (strlen($device)>0 || strlen($browser)>0) { ?>
+        <h2>What you told me...</h2>
+      <?php } else { ?>
+        <h2>You didn't tell me anything :( (No worries)</h2>
+      <?php } ?>
       <?php if (strlen($device)>0) { ?>
-        <b>Device:</b> <?php echo $device; ?> <br>
+      <p>
+        <b>Device:</b> <?php echo $device; ?>
+      </p>
       <?php } ?>
       <?php if (strlen($browser)>0) { ?>
+      <p>
         <b>Browser:</b> <?php echo $browser; ?> <br>
+      </p>
       <?php } ?>
-      <b>User Agent:</b> <?php echo $useragent; ?>
-    </p>
-
-    <p>
-      <b>Window width: </b>
-      <script type="text/javascript">
-      document.write(window.innerWidth);
-      </script>
-    </p>
+      <h2>Here's what the browser is telling me it is...</h2>
+      <p>
+        <?php echo $useragent; ?>
+      </p>
+    </div>    
+    <footer>
+      Website created by Sam Gregory.
+    </footer>
   </div>
 </body>
 </html>
